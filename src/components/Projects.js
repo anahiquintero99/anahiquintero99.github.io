a0b0bdf78c2a50169fe7757c554842a88af3eb3e
Anahi Quintero <anitaquintero99@gmail.com>
2022-10-16 11:39:13 -0500

    New project website

---
+++ src/components/Projects.js
@@ -0,0 +1,65 @@
+import { Col, Container, Row, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Visolutions",
+      description: "Visolutions es una empresa de inteligencia artificial",
+      imgUrl: projImg1,
+    },
+    {
+      title: "English Colors",
+      description: "Pagina enfocada a niños para aprender ingles",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Best Food",
+      description: "Maquetación de página sobre comida",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Proyectos</h2>
+            <p></p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                className="nav-pills mb-5 justify-content-center align-items-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first"> Proyectos </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">GitHub</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  Visita mi GitHub en donde podrás disfrutar más a detalle sobre
+                  mis proyectos en los cuales he trabajado.
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
